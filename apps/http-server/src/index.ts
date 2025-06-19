97b6e311e1c2fce4048a830fca5ddecfd1fe80e0
aashxx <tmohamedaashir@gmail.com>
2025-06-19 23:05:37 +0530

    added prisma client - 35:11

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -3,21 +3,41 @@ import jwt from 'jsonwebtoken';
 import { JWT_SECRET } from "@repo/server-common/config";
 import { createUserSchema, loginUserSchema } from "@repo/common/types";
 import { middleware } from "./middleware";
+import { prismaClient } from "@repo/db/client";
+import cors from "cors";
 
 const app = express();
 const PORT = 5002;
 
-app.post('/signup', (req, res) => {
-  const data = createUserSchema.safeParse(req.body);
-  if(!data.success) {
+app.use(express.json());
+app.use(cors());
+
+app.post('/signup', async (req, res) => {
+  const parsedData = createUserSchema.safeParse(req.body);
+  if(!parsedData.success) {
     res.json({
       message: "Failed to sign up",
     })
   }
-
-  res.json({
-    success: true,
-  })
+  try {
+    await prismaClient.user.create({
+      data: {
+        //@ts-ignore
+        fullName: parsedData.data.fullName,
+        //@ts-ignore
+        email: parsedData.data.email,
+        //@ts-ignore
+        password: parsedData.data.password
+      }
+    });
+    res.json({
+      success: true,
+    })
+  } catch (error) {
+    res.status(411).json({
+      message: "User already exists",
+    });
+  }
 });
 
 app.post('/signin', (req, res) => {
